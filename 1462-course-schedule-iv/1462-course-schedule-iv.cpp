b1cf0d714b95f5441ed140b87d52fdd05e8942e2
Aman Saini <sainiaman230905@gmail.com>
2026-06-29 23:35:03 +0530

    Time: 161 ms (13.33%), Space: 17.2 MB (99.84%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<bool> checkIfPrerequisite(int num, vector<vector<int>>& pre,
+                                     vector<vector<int>>& q) {
+        vector<vector<bool>> vec(num, vector<bool>(num, false));
+        for (int i = 0; i < num; i++) {
+
+            vec[i][i] = true;
+        }
+        for (auto& v : pre) {
+            vec[v[0]][v[1]] = true;
+        }
+        for (int k = 0; k < num; k++) {
+            for (int i = 0; i < num; i++) {
+                for (int j = 0; j < num; j++) {
+                    if (i == k or j == k)
+                        continue;
+                    if (i == j)
+                        continue;
+                    if (!vec[i][j]) {
+                        vec[i][j] = vec[i][k] and vec[k][j];
+                    }
+                }
+            }
+        }
+
+        vector<bool> ans;
+        for (auto& v : q) {
+            ans.push_back(vec[v[0]][v[1]]);
+        }
+
+        return ans;
+    }
+};
