afbe0dea0895c61176ac66f19434aae075e17888
Aman Saini <sainiaman230905@gmail.com>
2026-08-21 23:33:05 +0530

    Time: 43 ms (89.14%), Space: 18.7 MB (69.24%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -3,32 +3,20 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        unordered_set<int> st;
-        int lastrow = INT_MIN;
-        for (int i = 0; i < m; i++) {
-            lastrow = i;
-            if (matrix[i][0] > target) {
-                break;
-            }
-        }
-        if (lastrow == INT_MIN)
-            return false;
-        int lastcol = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            lastcol = i;
-            if (matrix[0][i] > target) {
-                break;
-            }
-        }
-        if (lastcol == INT_MIN)
-            return false;
 
-        for (int i = 0; i <= lastrow; i++) {
-            for (int j = 0; j <= lastcol; j++) {
-                if (matrix[i][j] == target)
-                    return true;
+        int i = m-1;
+        int j = 0;
+        while(i>=0 and j<n){
+            if(matrix[i][j]<target){
+                j++;
+            }else if(matrix[i][j]>target){
+                i--;
+            }else{
+                return true;
             }
         }
+
+
         return false;
     }
 };
